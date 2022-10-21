e8502367d506d8493796c1ae9751c8763833d230
ST10129875 <115506048+ST10131083@users.noreply.github.com>
2022-10-21 12:04:10 +0200

    Add project files.

---
+++ APPR6312_ST10129875/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace APPR6312_ST10129875.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
